ce7cbe86387772a636f4cdbf911012a183adebac
Chris Curran <chriscurran8@yahoo.com>
2022-04-20 11:49:40 +0000

    1st commit - 1st query function written

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  // const fetchData = async () => {
+  //   const response = await fetch()
+  // }
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App;
